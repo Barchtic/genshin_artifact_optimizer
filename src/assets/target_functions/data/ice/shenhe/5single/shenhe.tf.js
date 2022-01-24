d20f45d767627e227fd7c1a5d714d945094cfe00
BunnyHunter031 <bunnyhunter031@gmail.com>
2022-01-23 21:04:21 -0400

    main

---
+++ src/assets/target_functions/data/ice/shenhe/5single/shenhe.tf.js
@@ -0,0 +1,16 @@
+import badge from "@asset/badges/shenhe.png";
+import config from "./Shenhe.tcfg";
+
+export default {
+    name: "shenhe",
+    chs: "Spring Spirit Summoning",
+    description: [
+        "Maximize ATK"
+    ],
+    tags: [
+        "Shenhe",
+    ],
+    "for": "shenhe",
+    config,
+    badge,
+}
